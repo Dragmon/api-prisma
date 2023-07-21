a2e7be5726aafe556b38574d72d2984ca73352d1
Carlos Perez <carlos.perez@charliegperez.com>
2023-07-20 19:52:35 -0600

    Feature: added functionality to add and edit subaccounts

--- src/routes/subaccounts.routes.js
+++ src/routes/subaccounts.routes.js
@@ -8,11 +8,11 @@ router.get('/subaccounts', async (req, res) => {
   res.json(subAccounts);
 });
 
-router.get('/subaccounts/:rfc', async (req, res) => {
+router.get('/subaccounts/:order_account', async (req, res) => {
   console.log('req.params', req.params);
   const subAccountFound = await prisma.subaccount.findMany({
     where: {
-      rfc: req.params.rfc,
+      order_account: req.params.order_account,
     },
   });
   if (!subAccountFound) {
@@ -23,30 +23,59 @@ router.get('/subaccounts/:rfc', async (req, res) => {
 });
 
 router.post('/subaccounts', async (req, res) => {
-  const newSubAccount = await prisma.subaccount.create({
-    data: req.body,
-  });
-  res.json(newSubAccount);
+  try {
+    const newSubAccount = await prisma.subaccount.create({
+      data: req.body,
+    });
+    res.json(newSubAccount);
+  } catch (e) {
+    console.log('error', e);
+    if (e.code === 'P2002') {
+      return res
+        .status(400)
+        .json({ error: 'El RFC ya existe', target: e.meta.target });
+    } else {
+      return res.status(400).json({ name: e.name, error: e.message });
+    }
+  }
 });
 
-router.delete('/subaccounts/:rfc', async (req, res) => {
-  const subAccountDelete = await prisma.subaccount.delete({
-    where: {
-      rfc: req.params.rfc,
-    },
-  });
-  if (!subAccountDelete)
-    return res.status(404).json({ error: 'Product not found' });
+router.put('/subaccounts', async (req, res) => {
+  try {
+    const updateSubAccount = await prisma.subaccount.update({
+      where: {
+        id_subaccount: req.body.id_subaccount,
+      },
+      data: req.body,
+    });
+    return res.json(updateSubAccount);
+  } catch (e) {
+    console.log('error', e);
+    return res.status(400).json({ name: e.name, message: e.message });
+  }
 });
 
-router.put('/subaccounts/:rfc', async (req, res) => {
-  const subAccountUpdate = await prisma.subaccount.update({
-    where: {
-      rfc: req.params.rfc,
-    },
-    data: req.body,
-  });
-  return res.json(subAccountUpdate);
+router.delete('/subaccounts/:id_subaccount', async (req, res) => {
+  console.log('req', req.params.id_subaccount);
+  console.log('req', typeof req.params.id_subaccount);
+  try {
+    //const subAccountDelete = await prisma.account.delete({
+    const subAccountDelete = await prisma.account.findFirst({
+      where: {
+        id_subaccount: parseInt(req.params.id_subaccount),
+      },
+    });
+    res.json(subAccountDelete);
+  } catch (e) {
+    console.log('error', e);
+    console.log('error name', e.name);
+    console.log('error message', e.message);
+    if (e.code === 'P2025') {
+      return res.status(400).json({ error: 'El registro no existe' });
+    } else {
+      return res.status(400).json({ name: e.name, error: e.message });
+    }
+  }
 });
 
 export default router;
